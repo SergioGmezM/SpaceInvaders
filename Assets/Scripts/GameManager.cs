be91fb31651b985e5f3380ba8446868fc21417b7
TheBender10 <sergiogomez9867@gmail.com>
2021-09-18 14:13:29 +0200

    Added score functionality and optimized code

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int score = 0;
+
+    public void AddScore(int points)
+    {
+        score += points;
+        // Mostrar en una interfaz
+        Debug.Log(score);
+    }
+}
